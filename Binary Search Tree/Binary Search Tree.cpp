d1ad7244c640f59601eae35193b6b6379b8045a1
GurvirSingh <charizardzx9@gmail.com>
2020-02-01 15:02:30 -0500

    Created BSTree class in header file

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -3,12 +3,19 @@
 
 int main()
 {
+    
+    
+    
+    
+    
+    /*
     Node MainNode;
     MainNode.NodeValue = 8;
     Node Node2;
     Node2.NodeValue = 3;
-
     MainNode.AppendNode(Node2);
     std::cout << MainNode.leftNode->NodeValue;
+    */
+
 }
 
