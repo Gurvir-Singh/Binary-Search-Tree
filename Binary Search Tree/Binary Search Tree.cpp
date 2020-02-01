0e8e49e3016cd2268308a1fdcb8e8769048c5178
GurvirSingh <charizardzx9@gmail.com>
2020-02-01 12:10:41 -0500

    Added Node class implementation.

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -6,7 +6,7 @@ int main()
     Node MainNode;
     MainNode.NodeValue = 8;
     Node Node2;
-    Node2.NodeValue = 2;
+    Node2.NodeValue = 3;
 
     MainNode.AppendNode(Node2);
     std::cout << MainNode.leftNode->NodeValue;
